394e7b3e73a78d0b4170a96dc7fa0ec53c6fbbd4
Atle Magnussen <atlemagnussen@gmail.com>
2021-05-09 12:38:49 +0200

    umo

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-	export let name: string;
+	import { dosomething } from "./test"
+	export let name: string
+	const whut = dosomething()
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>{whut}</p>
 </main>
 
 <style>
--- workbox-config.js
+++ workbox-config.js
@@ -1,20 +1,12 @@
 export default {
     globDirectory: "public/",
     globPatterns: ["**/*.{css,html,js}"],
-    globFollow: true, // follow symlinks
-    globStrict: true, // fail the build if anything goes wrong while reading the files
-    globIgnores: [
-      // Ignore Angular's ES5 bundles
-      // With this, we eagerly load the es2015
-      // bundles and we only load/cache the es5 bundles when requested
-      // i.e., on browsers that need them
-      // Reference: https://github.com/angular/angular/issues/31256#issuecomment-506507021
-      `**/*-es5.*.js`,
-    ],
-    // Look for a 20 character hex string in the file names
-    // Allows to avoid using cache busting for Angular files because Angular already takes care of that!
-    // dontCacheBustURLsMatching: new RegExp(".+.[a-f0-9]{20}..+"),
-    maximumFileSizeToCacheInBytes: 4 * 1024 * 1024, // 4Mb
+    // globIgnores: ["sw.js", "workbox*.js"],
+    modifyURLPrefix: { 
+        "dist/": "/dist/",
+        "index.html": "/index.html"
+    },
+    maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
     swSrc: "public/dist/sw.js",
     swDest: "public/dist/sw.js",
 }
